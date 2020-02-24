0c4bd0359b626daa82c03fc402ab1c015a283703
Marc Aron Bøgh Barnholdt <mba@leneo.io>
2020-02-24 10:30:37 +0100

    Add project files.

---
+++ ClipboardStrip/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using TextCopy;
+
+namespace ClipboardStrip
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            if (Clipboard.GetText() is string text)
+            {
+                if (JsonSerializerExtensions.TryDeserialize(text, out dynamic json))
+                {
+                    text = JsonSerializer.Serialize<dynamic>(json, new JsonSerializerOptions
+                    {
+                        WriteIndented = true,
+                    });
+                }
+
+                Clipboard.SetText(text);
+            }
+        }
+    }
+
+    internal static class JsonSerializerExtensions
+    {
+        [SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Try pattern")]
+        public static bool TryDeserialize(string value, out dynamic json)
+        {
+            try
+            {
+                json = JsonSerializer.Deserialize<dynamic>(value);
+                return true;
+            }
+            catch (JsonException)
+            {
+                json = new object();
+                return false;
+            }
+        }
+    }
+}
